7525929ba2fb31dae9e48b09c6e57929dd18b420
Joyce Cho <joyjoycho@gmail.com>
2019-01-26 19:52:47 -0800

    delete unnecessary files

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,36 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import $ from 'jquery';
-import List from './components/List.jsx';
+import React from "react";
+import ReactDOM from "react-dom";
+import $ from "jquery";
+import List from "./components/List.jsx";
+import "bootstrap/dist/css/bootstrap.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       items: []
-    }
+    };
   }
 
   componentDidMount() {
     $.ajax({
-      url: '/items', 
-      success: (data) => {
+      url: "/items",
+      success: data => {
         this.setState({
           items: data
-        })
+        });
       },
-      error: (err) => {
-        console.log('err', err);
+      error: err => {
+        console.log("err", err);
       }
     });
   }
 
-  render () {
-    return (<div>
-      <h1>Item List</h1>
-      <List items={this.state.items}/>
-    </div>)
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">hello world</div>
+          <div className="col">hello world</div>
+        </div>
+        <List items={this.state.items} />
+      </div>
+    );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
